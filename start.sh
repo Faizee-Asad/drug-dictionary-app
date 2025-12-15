954c980ff224ba6f0c53ed69f6452b4835821603
Faizee Asad <72010857+Faizee-Asad@users.noreply.github.com>
2025-12-15 15:43:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
 # Navigate to the backend directory and start the application
 cd backend
 exec python -m uvicorn app.main:app --host 0.0.0.0 --port $PORT
-=======
-export PYTHONPATH=$PYTHONPATH:/opt/render/project/src/backend
-uvicorn app.main:app --host 0.0.0.0 --port $PORT
->>>>>>> 2853a3f237991fe0b366ebce744d1a1df4cd7e25
