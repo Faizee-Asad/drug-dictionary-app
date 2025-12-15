2853a3f237991fe0b366ebce744d1a1df4cd7e25
Faizee Asad <72010857+Faizee-Asad@users.noreply.github.com>
2025-12-15 15:18:57 +0530

    Update PYTHONPATH and modify uvicorn command

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-uvicorn backend.app.main:app --host 0.0.0.0 --port $PORT
+export PYTHONPATH=$PYTHONPATH:/opt/render/project/src/backend
+uvicorn app.main:app --host 0.0.0.0 --port $PORT
